9ce67b4c1242f60cea4fe3b56cf076d46125e75c
Dilxush-Orziqulov <orziqulovdilxush@gmail.com>
2024-03-14 17:59:02 +0500

    board add

--- js/app.js
+++ js/app.js
@@ -1,22 +1,64 @@
 let search_btn = document.querySelector('.search_btn');
+let del = document.querySelector('.del');
+let searchbox = document.getElementById('search');
+let board = document.querySelector('.board');
+let board_btn = document.querySelector('.board-btn');
 
-search_btn.addEventListener('click', search = ()=>{
+search_btn.addEventListener('click', search = () => {
   const searchbox = document.getElementById('search').value.toUpperCase();
   const storeitems = document.getElementById('list');
   const multik_item = document.querySelectorAll('.multik_item');
   const pname = storeitems.getElementsByTagName('h4');
+  board.style.display = 'none';
+
 
   for (let i = 0; i < pname.length; i++) {
-      const match = multik_item[i].getElementsByTagName('h4')[0];
+    const match = multik_item[i].getElementsByTagName('h4')[0];
 
-      if (match) {
-          let textvalue = match.textContent || match.innerHTML;
+    if (match) {
+      let textvalue = match.textContent || match.innerHTML;
 
-          if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
-            multik_item[i].style.display = '';
-          } else {
-            multik_item[i].style.display = 'none';
-          }
+      if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+        multik_item[i].style.display = '';
+      } else {
+        multik_item[i].style.display = 'none';
+        board.style.display = 'none';
       }
+    }
   }
+});
+
+board_btn.addEventListener('click', search = () => {
+  const searchbox = document.getElementById('search').value.toUpperCase();
+  const storeitems = document.getElementById('list');
+  const multik_item = document.querySelectorAll('.multik_item');
+  const pname = storeitems.getElementsByTagName('h4');
+  board.style.display = 'none';
+
+
+  for (let i = 0; i < pname.length; i++) {
+    const match = multik_item[i].getElementsByTagName('h4')[0];
+
+    if (match) {
+      let textvalue = match.textContent || match.innerHTML;
+
+      if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+        multik_item[i].style.display = '';
+      } else {
+        multik_item[i].style.display = 'none';
+        board.style.display = 'none';
+      }
+    }
+  }
+});
+
+del.addEventListener('click', () => {
+  searchbox.value = searchbox.value.slice(0, -1)
 })
+
+searchbox.addEventListener('click', () => {
+  board.style.display = 'flex'
+})
+
+
+
